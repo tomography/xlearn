45e6d829051f93eeb22e1140cff073c9ca14a6ea
Francesco De Carlo <decarlof@gmail.com>
2018-06-28 16:09:17 -0500

    fixed the docs

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-#    'nbsphinx',
+    'nbsphinx',
 #    'IPython.sphinxext.ipython_console_highlighting',
 #    'IPython.sphinxext.ipython_directive',
     'sphinx.ext.autodoc',
